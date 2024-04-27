9992243efb99fff9df53ec428ff6003bed7fc1cf
sheshankpriyadarshi <sheshankpriyadarshi@gmail.com>
2024-04-27 18:24:22 +0530

    Prepared the sales report using both sql and pandas

---
+++ sales_report_pandas.py
@@ -0,0 +1,36 @@
+from sqlalchemy import create_engine
+import pandas as pd
+import csv
+
+try:
+    # Connect to the SQLite database
+    engine = create_engine('sqlite:///Data Engineer_ETL Assignment.db')
+
+    # Read the sales, cutomer and orders table into a Pandas DataFrame
+    sales_df = pd.read_sql_table('sales', engine)
+    customer_df = pd.read_sql_table('customers', engine)
+    orders_df = pd.read_sql_table('orders', engine)
+
+    # Joining customer and sales table on the basis of cutomer_id
+    customer_sales_df = pd.merge(customer_df, sales_df, on='customer_id')
+    # Filtering age betwween 18 to 35
+    filtered_customer_df =  customer_sales_df.where(customer_sales_df['age'].between(18, 35))
+    # Removing Nan values
+    filtered_customer_df = filtered_customer_df.dropna()
+    # Joining orders table with filtered_customer dataframe
+    order_customer_df = pd.merge(orders_df, filtered_customer_df, on='sales_id')
+    # Replacing Nan values present in quantity column with 0
+    order_customer_df['quantity'] = order_customer_df['quantity'].fillna(0)
+    # Adding the quantites for each unique items represented by item_id for the given customers
+    sales_report_df = order_customer_df.groupby(['item_id', 'customer_id']).agg({'age':'first', 'quantity': 'sum'})
+    # Taking quantities only greater than 0
+    sales_report_df = sales_report_df[sales_report_df['quantity']>0]
+    # Convering the age and quantity column values to integer type
+    sales_report_df['age'] = sales_report_df['age'].astype(int)
+    sales_report_df['quantity'] = sales_report_df['quantity'].astype(int)
+    # Saving the final dataframe as csv
+    sales_report_df.to_csv('pandas_result.csv', sep=';')
+except Exception as e:
+    print(f'Error --- {e}')
+finally: 
+    engine.dispose()
---
+++ sales_report_sql.py
@@ -0,0 +1,31 @@
+import sqlite3 as sql_engine
+import sys
+import csv
+
+try: 
+    # Establishing connection with database
+    con = sql_engine.connect('Data Engineer_ETL Assignment.db')
+    # Initializing cursor object to execute SQL queries 
+    cur = con.cursor()     
+
+    customer_age_table = cur.execute("SELECT filtered_sales.customer_id, filtered_sales.age, filtered_sales.item_id, CAST(SUM(COALESCE(filtered_sales.quantity, 0)) AS INTEGER) as total_quantity FROM (SELECT *  FROM (SELECT s.sales_id, s.customer_id, c.age FROM sales AS s JOIN customers AS c ON s.customer_id = c.customer_id WHERE c.age BETWEEN 18 AND 35) AS filtered_customers JOIN orders AS o ON filtered_customers.sales_id = o.sales_id) as filtered_sales GROUP BY filtered_sales.item_id, filtered_sales.customer_id HAVING total_quantity > 0  LIMIT 50;")
+    # Fetch all rows
+    rows = cur.fetchall()
+
+    csv_file_path = 'sql_result.csv'
+
+    # Write the fetched rows to a CSV file with ';' as the delimiter
+    with open(csv_file_path, 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile, delimiter=';')
+        # Write the header
+        csv_writer.writerow(['Customer', 'Age', 'Item', 'Quantity'])
+        # Write the rows
+        csv_writer.writerows(rows)
+except Exception as e: 
+    if con: 
+        con.rollback()
+    print(f'Error --- {e}')
+    sys.exit(1) 
+finally: 
+    if con: 
+        con.close()  
